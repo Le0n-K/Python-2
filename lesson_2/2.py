ed2f1341861ebfd2b2a347f8f11eb502d5e33b4a
leon <leon.kushnir15@gmail.com>
2023-12-07 16:00:13 +0200

    tariffs

---
+++ lesson_2/2.py
@@ -0,0 +1,5 @@
+
+a = int(input('Your water readings for the past month:'))
+b = int(input('Your water readings for the current month:'))
+c = int(input('What is your tariff?'))
+print(f'It is your results: {(a - b) * c}')
